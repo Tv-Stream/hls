4425af67b4f21fd74f88d5ee37bb3735e1303f6e
Tv-Stream <39259573+Tv-Stream@users.noreply.github.com>
2018-05-16 09:19:15 +0700

    Create auto.js

---
+++ swft/js/auto.js
@@ -0,0 +1,5 @@
+    var player = videojs('video');
+    player.src({
+      src: 'https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/playlist.m3u8',
+      type: 'application/vnd.apple.mpegurl'
+    });
